660fa0feb781936c265aec5eb160015d98b1c2a9
Joseph Horning <joeytyndale@gmail.com>
2019-04-29 16:23:59 -0700

    Updated the course ID function to destroy ID after each operation. This prevents bug which stored and re-presented the first ID it gathered

--- firefox/courseid.js
+++ firefox/courseid.js
@@ -1,16 +1,22 @@
 if(document.getElementById('vjs_video_1_html5_api') !== null){
-	var elem = document.getElementById('vjs_video_1_html5_api').src;
+	elem = document.getElementById('vjs_video_1_html5_api').src;
 }
 if(document.getElementById('vjs_video_2_html5_api') !== null){
-	var elem = document.getElementById('vjs_video_2_html5_api').src;
+	elem = document.getElementById('vjs_video_2_html5_api').src;
 }
 if(document.getElementById('vjs_video_3_html5_api') !== null){
-	var elem = document.getElementById('vjs_video_3_html5_api').src;
+	elem = document.getElementById('vjs_video_3_html5_api').src;
 }
 if(document.getElementById('vjs_video_4_html5_api') !== null){
-	var elem = document.getElementById('vjs_video_4_html5_api').src;
+	elem = document.getElementById('vjs_video_4_html5_api').src;
 }
 
-var id = elem.slice(elem.indexOf("rses/") + 5,elem.indexOf("/",elem.indexOf("rses/") + 6));
+console.log(elem);
+console.log(window.location);
+
+id = elem.slice(elem.indexOf("rses/") + 5,elem.indexOf("/",elem.indexOf("rses/") + 6));
 
 alert("Course ID: " + id);
+
+delete elem;
+delete id;
