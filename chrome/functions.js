354f5af13538827016fe877deabac43cdd7a964e
Joseph Horning <jhorning@linkedin.biz>
2019-04-29 16:51:34 -0700

    Updated Chrome script with recent Firefox updates. Course ID fixes

--- chrome/functions.js
+++ chrome/functions.js
@@ -27,21 +27,27 @@ chrome.runtime.onMessage.addListener(function (msg, sender) {
     }
     else if (msg.text === 'courseId'){
 
-    	if(document.getElementById('vjs_video_1_html5_api') !== null){
-    		var elem = document.getElementById('vjs_video_1_html5_api').src;
-    	}
-    	if(document.getElementById('vjs_video_2_html5_api') !== null){
-    		var elem = document.getElementById('vjs_video_2_html5_api').src;
-    	}
-    	if(document.getElementById('vjs_video_3_html5_api') !== null){
-    		var elem = document.getElementById('vjs_video_3_html5_api').src;
-    	}
-    	if(document.getElementById('vjs_video_4_html5_api') !== null){
-    		var elem = document.getElementById('vjs_video_4_html5_api').src;
-    	}
+	if(document.getElementById('vjs_video_1_html5_api') !== null){
+		elem = document.getElementById('vjs_video_1_html5_api').src;
+	}
+	if(document.getElementById('vjs_video_2_html5_api') !== null){
+		elem = document.getElementById('vjs_video_2_html5_api').src;
+	}
+	if(document.getElementById('vjs_video_3_html5_api') !== null){
+		elem = document.getElementById('vjs_video_3_html5_api').src;
+	}
+	if(document.getElementById('vjs_video_4_html5_api') !== null){
+		elem = document.getElementById('vjs_video_4_html5_api').src;
+	}
 
-    	var id = elem.slice(elem.indexOf("rses/") + 5,elem.indexOf("/",elem.indexOf("rses/") + 6));
+	console.log(elem);
+	console.log(window.location);
 
-    	prompt("Course ID: ", id);
+	id = elem.slice(elem.indexOf("rses/") + 5,elem.indexOf("/",elem.indexOf("rses/") + 6));
+
+	alert("Course ID: " + id);
+
+	delete elem;
+	delete id;    
     }
 });
