935f1a61b1205ccf509896ddfd2a1eda207bd447
Joseph Horning <jhorning@linkedin.biz>
2020-08-28 07:28:03 -0700

    JH removed features that overlap with CSP Extension features. Cleaned up some code

--- chrome/background.js
+++ chrome/background.js
@@ -6,37 +6,16 @@ function openProfile(){
 
 
 chrome.runtime.onInstalled.addListener(function(){
-	var id = chrome.contextMenus.create({
+    chrome.contextMenus.create({
 	    "title":"Get Course ID", 
 	    "contexts": ["all"], 
 	    "id": "courseId"
-    });
-    var id = chrome.contextMenus.create({
-	    "title":"Grab Ent Info", 
-	    "contexts": ["all"], 
-	    "id": "getInfo"
-    });
-    var id = chrome.contextMenus.create({
-	    "title":"Open Profile", 
-	    "contexts": ["all"], 
-	    "id": "openProfile"
     });
 });
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 function onClickHandler(info,tab){
-	if(info.menuItemId == "getInfo"){
-		chrome.tabs.sendMessage(tab.id, {text: 'getInfo'});
-	}
-	else if (info.menuItemId == "openProfile"){
-		chrome.tabs.sendMessage(tab.id, {text: 'openProfile'});
-	}
-	else if (info.menuItemId == "courseId"){
-		chrome.tabs.sendMessage(tab.id, {text: 'courseId'})
-	}
-	else{
-		alert("I don't know what you just did");
-	}
+	chrome.tabs.sendMessage(tab.id,{text:info.menuItemId});
 };
 
--- chrome/functions.js
+++ chrome/functions.js
@@ -1,31 +1,7 @@
 
 
 chrome.runtime.onMessage.addListener(function (msg, sender) {
-    // If the received message has the expected format...
-    if (msg.text === 'openProfile') {
-
-        var liasForm = document.getElementsByClassName('lias-form')[0];
-
-		var liasLink = liasForm.getAttribute('action');
-
-		var profileId = liasLink.substring(liasLink.indexOf('ProfileId=') + 10, liasLink.indexOf('&amp;enterpriseApplication'));
-
-		var accountId = liasLink.substring(liasLink.indexOf('Seat/') + 5, liasLink.indexOf('?enterpriseProfileId'));
-
-		window.open(`https://customer.www.linkedin.com/support/account/urn:li:enterpriseAccount:${accountId}/profiles/${profileId}/licenses`);
-
-    }
-    else if (msg.text === 'getInfo'){
-
-    	var url = window.location.href;
-		var accountId = url.substring(url.indexOf("enterpriseAccount:") + 18,url.indexOf("/profiles"));
-		var profileId = url.substring(url.indexOf("profiles/") + 9,url.indexOf("/licenses"));
-
-		prompt("Enterprise Info", "• Profile ID: " + profileId + "\n• Account ID: " + accountId);
-
-		//window.open("https://customer.www.linkedin.com/support/product/urn:li:enterpriseApplicationInstance:(urn:li:enterpriseAccount:" + acountId + ")");
-    }
-    else if (msg.text === 'courseId'){
+    if (msg.text === 'courseId'){
 
 	if(document.getElementById('vjs_video_1_html5_api') !== null){
 		elem = document.getElementById('vjs_video_1_html5_api').src;
